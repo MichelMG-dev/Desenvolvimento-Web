c9708516671cf4d2ef5fc0bfe1c0db6dbbb60719
Michel de Melo G. <michelmg.dev145@gmail.com>
2024-11-01 17:12:11 -0300

    site botafogo

---
+++ semana8/detalhes.js
@@ -0,0 +1,26 @@
+const params = new URLSearchParams(window.location.search)
+
+const pega_json = async(caminho) =>{
+    const resposta = await fetch(caminho);
+    const dados = await resposta.json();
+    return dados;
+}
+
+const montaPagina = (dados) => {
+    const body = document.body;
+    body.innerHTML = '';
+
+    const nome = document.createElement('h1');
+    nome.innerHTML = dados.nome;
+    body.appendChild(nome);
+
+    const imagem = document.createElement('img');
+    imagem.src = dados.imagem;
+    body.appendChild(imagem);
+}
+
+pega_json(`https://botafogo-atletas.mange.li/2024-1/${params.get("id")}`).then(
+    (r) => montaPagina(r)
+);
+
+console.log(params.get("id"));
